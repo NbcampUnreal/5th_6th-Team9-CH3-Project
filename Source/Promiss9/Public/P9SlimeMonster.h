4c4ff2a597abf8bdc811482b6946349180e43240
Jaeuk No <127023656+NoJaeuk@users.noreply.github.com>
2025-10-24 15:40:51 +0900

    add_P9Slime

---
+++ Source/Promiss9/Public/P9SlimeMonster.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "P9Monster.h"
+#include "P9SlimeMonster.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROMISS9_API AP9SlimeMonster : public AP9Monster
+{
+	GENERATED_BODY()
+	
+};
