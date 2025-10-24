0d14d0e94602f584a217bf8982042f187443f90b
ggyu-ik <ggye0723@gmail.com>
2025-10-24 21:55:32 +0900

    파일 정리

---
+++ Source/Promiss9/Private/P9PlayerController.cpp
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "P9PlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "Blueprint/UserWidget.h"
+
+AP9PlayerController::AP9PlayerController()
+	:InputMappingContext(nullptr),
+	MoveAction(nullptr),
+	JumpAction(nullptr),
+	LookAction(nullptr),
+	ForwardRollAction(nullptr),
+	InteractAction(nullptr),
+	HUDWidgetClass(nullptr),
+	HUDWidgetInstance(nullptr)
+{
+}
+
+void AP9PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (ULocalPlayer* LocalPlayer = GetLocalPlayer())
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* SubSystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+		{
+			if (InputMappingContext)
+			{
+				SubSystem->AddMappingContext(InputMappingContext, 0);
+			}
+		}
+	}
+}
+
+UUserWidget* AP9PlayerController::GetHUDWidget() const
+{
+	return HUDWidgetInstance;
+}
+
+void AP9PlayerController::ShowGameHUD()
+{
+	if (HUDWidgetInstance)
+	{
+		HUDWidgetInstance->RemoveFromParent();
+		HUDWidgetInstance = nullptr;
+	}
+
+	if (HUDWidgetClass)
+	{
+		HUDWidgetInstance = CreateWidget<UUserWidget>(this, HUDWidgetClass);
+		if (HUDWidgetInstance)
+		{
+			HUDWidgetInstance->AddToViewport();
+
+			bShowMouseCursor = false;
+			SetInputMode(FInputModeGameOnly());
+		}
+
+	}
+}
+	
+
+
---
+++ Source/Promiss9/Public/P9GameMode.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/ObjectPtr.h"
+#include "P9GameState.h"
+#include "GameFramework/GameModeBase.h"
+#include "P9GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROMISS9_API AP9GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+private:
+
+	UPROPERTY()
+	TObjectPtr<AP9GameState> P9GameState;
+};
