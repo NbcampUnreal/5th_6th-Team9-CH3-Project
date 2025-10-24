13265d30f1e0c540b2cd514fffd0c437d54a37a6
YUYEON91 <cybott2002@gmail.com>
2025-10-24 22:14:52 +0900

    Revert "Merge pull request #19 from NbcampUnreal/UI_feature"
    This reverts commit 1f58d7205d2b36209ae981ef4c40686217fd54a2, reversing
    changes made to d88618372c207ec58ef4a8b829fc51c1f185fd24.

--- Source/Promiss9/Private/P9GameMode.cpp
+++ Source/Promiss9/Private/P9GameMode.cpp
@@ -1,5 +1,32 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿#include "P9GameMode.h"
 
+void AP9GameMode::BeginPlay()
+{
+	Super::BeginPlay();
 
-#include "P9GameMode.h"
+	P9GameState = GetGameState<AP9GameState>();
+	if (P9GameState != nullptr)
+	{
+		GetWorld()->GetTimerManager().SetTimer(WaveTimer, this, &AP9GameMode::UpdateTimer, 1.0f, true);
 
+		P9GameState->SetCurrentWave(0);
+	}
+}
+
+void AP9GameMode::UpdateTimer()
+{
+	if (P9GameState == nullptr) return;
+	
+	P9GameState->CurrentGameTime += 1.0f;
+	int32 CurrentWave = P9GameState->CurrentWaveIndex;
+
+	if (P9GameState->WaveSettings.IsValidIndex(CurrentWave))
+	{
+		float CurrentWaveEndTime = P9GameState->WaveSettings[CurrentWave].WaveEndTime;
+
+		if (P9GameState->CurrentGameTime >= CurrentWaveEndTime)
+		{
+			P9GameState->NextWave();
+		}
+	}
+}
--- Source/Promiss9/Public/P9GameMode.h
+++ Source/Promiss9/Public/P9GameMode.h
@@ -1,6 +1,4 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "UObject/ObjectPtr.h"
@@ -20,4 +18,8 @@ private:
 
 	UPROPERTY()
 	TObjectPtr<AP9GameState> P9GameState;
+	FTimerHandle WaveTimer;
+
+	void UpdateTimer();
+	void BeginPlay() override;
 };
