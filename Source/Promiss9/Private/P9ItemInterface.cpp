35792e37a83358f4cebab5d0d7c4154a1e65be85
유용선(Unreal 6기) <dtoli0930@gmail.com>
2025-10-24 20:47:54 +0900

    [Add]Add ItemDataTaable
    무기 데이터 테이블 추가, 무기데이터 구조체 추가 BaseItem,ItemInterface 이름변경

---
+++ Source/Promiss9/Private/P9ItemInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "P9ItemInterface.h"
+
+// Add default functionality here for any IP9ItemInterface functions that are not pure virtual.
--- Source/Promiss9/Public/P9ItemActor.h
+++ Source/Promiss9/Public/P9ItemActor.h
@@ -4,28 +4,37 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "ItemInterface.h"
-#include "BaseItem.generated.h"
+#include "P9ItemInterface.h"
+#include "P9WeaponData.h"
+#include "P9ItemActor.generated.h"
+
 
 UCLASS()
-class PROMISS9_API ABaseItem : public AActor, public IItemInterface
+class PROMISS9_API AP9ItemActor : public AActor, public IP9ItemInterface
 {
 	GENERATED_BODY()
 
-public:	
+public:
 	// Sets default values for this actor's properties
-	ABaseItem();
+	AP9ItemActor();
 
 protected:
 	// Called when the game starts or when spawned
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	virtual void BeginPlay() override;
+
 	FName ItemType;
+
 
 	virtual void OnItemOverlap(AActor* OverlapActor) override;
 	virtual void OnItemEndOverlap(AActor* OverlapActor) override;
 	virtual void ActivateItem(AActor* Activator) override;
 	virtual FName GetItemType() const override;
 
-	virtual void DestroyItem();
+	virtual void DestroyIterm();
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
 
 };
