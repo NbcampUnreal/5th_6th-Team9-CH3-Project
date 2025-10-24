b585bee033e51a17cb5c0c6c7d74f6d06e20a4e1
Jaeuk No <127023656+NoJaeuk@users.noreply.github.com>
2025-10-24 16:23:51 +0900

    update_P9Monster_P9SlimeMonster
    Add BP_SlimeMonster, BT_SlimeMonster

--- Source/Promiss9/Private/P9SlimeMonster.cpp
+++ Source/Promiss9/Private/P9SlimeMonster.cpp
@@ -1,5 +1,19 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "P9SlimeMonster.h"
 
+AP9SlimeMonster::AP9SlimeMonster()
+{
+    // 슬라임 기본 스탯
+    HP = 50.f;             // 체력
+    AttackPower = 8.f;     // 공격력
+    ExpReward = 5;         // 경험치 드랍량
+    GoldReward = 2;        // 골드 드랍량
+    DamageInterval = 1.5f; // 공격 딜레이 간격
+}
+
+void AP9SlimeMonster::BeginPlay()
+{
+    Super::BeginPlay();
+
+    // 추가 초기화가 필요하면 여기서 처리 가능
+    // 예: 슬라임 전용 파티클, 사운드 등
+}
--- Source/Promiss9/Public/P9SlimeMonster.h
+++ Source/Promiss9/Public/P9SlimeMonster.h
@@ -1,17 +1,18 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "P9Monster.h"
 #include "P9SlimeMonster.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class PROMISS9_API AP9SlimeMonster : public AP9Monster
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
+public:
+    AP9SlimeMonster();
+
+protected:
+    virtual void BeginPlay() override;
 };
