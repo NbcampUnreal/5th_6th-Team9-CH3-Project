390e9b5f05f913d6958fd39971ed8387d5d5f588
JinYeon-Park <a9526191@gmail.com>
2025-10-25 18:20:36 +0900

    add : P9MonsterSpawner
    add :
    몹 스폰용 액터클래스 P9MonsterSpawner 추가

---
+++ Source/Promiss9/Public/P9MonsterSpawner.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "P9MonsterSpawner.generated.h"
+
+UCLASS()
+class PROMISS9_API AP9MonsterSpawner : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AP9MonsterSpawner();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
