abc4405bdf7ca46cc68f8d5cdad12bfab6dc0af0
JinYeon-Park <a9526191@gmail.com>
2025-10-27 16:50:20 +0900

    add : P9PlayerState 생성
    add :
    P9PlayerState 생성

---
+++ Source/Promiss9/Public/P9PlayerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "P9PlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROMISS9_API AP9PlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
