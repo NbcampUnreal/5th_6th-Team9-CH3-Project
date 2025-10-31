3371757651aa53890b76f8484ec6969ea9d4ecf7
JinYeon-Park <a9526191@gmail.com>
2025-10-31 10:46:28 +0900

    add : P9FinalBossAltar
    보스 스폰 제단용 액터 클래스 생성

---
+++ Source/Promiss9/Private/P9FinalBossAltar.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "P9FinalBossAltar.h"
+
+// Sets default values
+AP9FinalBossAltar::AP9FinalBossAltar()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AP9FinalBossAltar::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AP9FinalBossAltar::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
