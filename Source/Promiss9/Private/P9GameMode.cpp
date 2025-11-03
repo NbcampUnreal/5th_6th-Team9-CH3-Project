0e7152a431f5f9264a728d0b5ecb196ad12d8ae8
JinYeon-Park <a9526191@gmail.com>
2025-11-03 20:19:46 +0900

    update : P9GameMode 상점 스폰 로직 추가
    update :
    P9GameMode에 상점 스폰 로직 추가

--- Source/Promiss9/Private/P9GameMode.cpp
+++ Source/Promiss9/Private/P9GameMode.cpp
@@ -1,4 +1,12 @@
 ﻿#include "P9GameMode.h"
+#include "NavigationSystem.h"
+#include "Kismet/GameplayStatics.h"
+
+AP9GameMode::AP9GameMode()
+{
+	ShopRespawnTimer = ShopSpawnFrequency;
+	CurrentShop = nullptr;
+}
 
 void AP9GameMode::BeginPlay()
 {
@@ -13,8 +21,52 @@ void AP9GameMode::BeginPlay()
 	}
 }
 
+void AP9GameMode::SpawnShop()
+{
+	if (ShopClass == nullptr) return;
+
+	FVector SpawnLocation;
+
+	if (FindShopSpawnLocation(SpawnLocation))
+	{
+		CurrentShop = GetWorld()->SpawnActor<AP9Shop>(ShopClass, SpawnLocation, FRotator::ZeroRotator);
+	}
+}
+
+bool AP9GameMode::FindShopSpawnLocation(FVector& OutLocation)
+{
+	ACharacter* PlayerCharacter = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
+	if (PlayerCharacter == nullptr) return false;
+
+	const FVector PlayerLocation = PlayerCharacter->GetActorLocation();
+
+	UNavigationSystemV1* NavSystem = UNavigationSystemV1::GetCurrent(GetWorld());
+	if (NavSystem == nullptr) return false;
+
+	for (int32 i = 0; i < 10; ++i)
+	{
+		float RandomAngle = FMath::RandRange(0.0f, 360.0f);
+		FVector RandomDirection = FRotator(0.0f, RandomAngle, 0.0f).Vector();
+		float RandomDistance = FMath::RandRange(ShopMinSpawnRadius, ShopMaxSpawnRadius);
+
+		FVector SpawnPlace = PlayerLocation + (RandomDirection * RandomDistance);
+
+		FNavLocation ResultLocation;
+		bool bFound = NavSystem->ProjectPointToNavigation(SpawnPlace, ResultLocation, FVector(500.0f));
+
+		if (bFound)
+		{
+			OutLocation = ResultLocation.Location;
+			return true;
+		}
+	}
+
+	return false;
+}
+
 void AP9GameMode::UpdateTimer()
 {
+	//WaveSystem Timer
 	if (P9GameState == nullptr) return;
 	
 	P9GameState->CurrentGameTime += 1.0f;
@@ -29,4 +81,21 @@ void AP9GameMode::UpdateTimer()
 			P9GameState->NextWave();
 		}
 	}
+	//ShopSystem Timer
+	if (CurrentShop != nullptr) return;
+
+	ShopRespawnTimer -= 1.0f;
+
+	if (ShopRespawnTimer <= 0.0f)
+	{
+		SpawnShop();
+		ShopRespawnTimer = ShopSpawnFrequency;
+	}
+
+}
+
+void AP9GameMode::OnShopPurchased()
+{
+	CurrentShop = nullptr;
+	ShopRespawnTimer = ShopSpawnFrequency;
 }
--- Source/Promiss9/Public/P9GameMode.h
+++ Source/Promiss9/Public/P9GameMode.h
@@ -3,6 +3,7 @@
 #include "CoreMinimal.h"
 #include "UObject/ObjectPtr.h"
 #include "P9GameState.h"
+#include "P9Shop.h"
 #include "GameFramework/GameModeBase.h"
 #include "P9GameMode.generated.h"
 
@@ -14,11 +15,31 @@ class PROMISS9_API AP9GameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 
+public:
+	AP9GameMode();
+	void OnShopPurchased();
+
 private:
 
 	UPROPERTY()
 	TObjectPtr<AP9GameState> P9GameState;
 	FTimerHandle WaveTimer;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Shop")
+	TSubclassOf<AP9Monster> ShopClass;
+	UPROPERTY(EditDefaultsOnly, Category = "Shop")
+	float ShopSpawnFrequency = 20.0f;
+	UPROPERTY(EditDefaultsOnly, Category = "Shop")
+	float ShopMinSpawnRadius = 1000.0f;
+	UPROPERTY(EditDefaultsOnly, Category = "Shop")
+	float ShopMaxSpawnRadius = 5000.0f;
+
+	float ShopRespawnTimer;
+	TObjectPtr<AP9Shop> CurrentShop;
+	
+	void SpawnShop();
+
+	bool FindShopSpawnLocation(FVector& OutLocation);
 
 	void UpdateTimer();
 	void BeginPlay() override;
