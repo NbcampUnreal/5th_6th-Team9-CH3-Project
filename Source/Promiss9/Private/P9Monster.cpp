8a2351257a5d80af42255d6d3cc6962c038b6efa
Jaeuk No <127023656+NoJaeuk@users.noreply.github.com>
2025-11-06 17:33:56 +0900

    add 타이머 검사

--- Source/Promiss9/Private/P9Monster.cpp
+++ Source/Promiss9/Private/P9Monster.cpp
@@ -173,6 +173,7 @@ void AP9Monster::Die()
     }
    
     //1.33초 뒤에 디졸브 시작
+    if (IsActorBeingDestroyed()) return;
     FTimerHandle DeathTimerHandle;
     GetWorld()->GetTimerManager().SetTimer(DeathTimerHandle, [this]()
         {
@@ -251,6 +252,7 @@ void AP9Monster::ShowDamageWidget(float DamageValue, bool bIsCritical)
     TWeakObjectPtr<UWidgetComponent> WeakDamageWidget = DamageWidgetComp;
 
     // 카메라 바라보기 타이머
+    if (IsActorBeingDestroyed()) return;
     GetWorld()->GetTimerManager().SetTimer(
         FaceTimer,
         [WeakDamageWidget]()
@@ -269,6 +271,7 @@ void AP9Monster::ShowDamageWidget(float DamageValue, bool bIsCritical)
     );
 
     // 기존 제거 타이머 안에 FaceTimer 해제도 추가
+    if (IsActorBeingDestroyed()) return;
     GetWorld()->GetTimerManager().SetTimer(RemoveTimer, [this, WeakDamageWidget, &FaceTimer]()
         {
             if (!IsValid(this)) return;
